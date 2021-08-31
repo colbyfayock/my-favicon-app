e95c1c5f5c20af840ed58f3be11d0b6b1f269658
colbyfayock <fayock@gmail.com>
2021-08-31 14:12:38 -0400

    Step 4: dynamically updating the favicon on button click

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,39 @@
+import { useState } from 'react';
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [favicon, setFavicon] = useState('');
+  console.log(`favicon state: ${favicon}`);
+  function handleOnClick() {
+    setFavicon('favorite');
+    setTimeout(() => setFavicon(''), 3000);
+  }
   return (
     <div className={styles.container}>
       <Head>
-        <title>Admin</title>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <title>Home</title>
+        <link rel="icon" type="image/png" sizes="32x32" href={favicon ? `/favicon-${favicon}-32x32.png` : `/favicon-32x32.png`} />
+        <link rel="icon" type="image/png" sizes="16x16" href={favicon ? `/favicon-${favicon}-16x16.png` : `/favicon-16x16.png`} />
       </Head>
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <p>
+          <button onClick={handleOnClick} style={{
+            color: 'white',
+            backgroundColor: 'blueviolet',
+            padding: '.6em .8em',
+            border: 0,
+            cursor: 'pointer'
+          }}>
+            ❤️ Smash that Like button!
+          </button>
+        </p>
 
         <p>
           <Link href="/admin">
